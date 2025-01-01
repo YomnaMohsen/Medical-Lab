42ce38470738232f0eecdadcdc72ff2827d251f8
YomnaMohsen <ymn_mohsen@yahoo.com>
2025-01-01 17:38:43 +0200

    update_passord_version2

--- routes/userroutes.js
+++ routes/userroutes.js
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { application } from 'express';
 import doctorModel from '../models/Doctor.js';
 import patientModel from '../models/Patient.js';
 import authController from '../controllers/authController.js';
@@ -11,14 +11,16 @@ const userRouter = express.Router();
 userRouter.post("/patient/login", authController.Patlogin);
 userRouter.post("/doctor/login", authController.Doclogin);
 
+
+application.use(authUser, alloweduser(doctorModel));
 // doctor protected routes
-userRouter.patch("/doctor/updatePassword", authUser, alloweduser(doctorModel), userController.updatePassword(doctorModel));
-userRouter.post("/doctor/test", authUser, alloweduser(doctorModel), userController.addTest);
-userRouter.get("/doctor/test/:id", authUser, alloweduser(doctorModel), userController.getTest);
-userRouter.patch("/doctor/test/:id", authUser, alloweduser(doctorModel), userController.updateTest);
-userRouter.delete("/doctor/test/:id", authUser, alloweduser(doctorModel), userController.deleteTest);
-userRouter.get("/doctor/tests/:doctorid", authUser, alloweduser(doctorModel), userController.getTestsbyDoctor);
-userRouter.get("/doctor/Alltests/:patientid/:doctorid", authUser, alloweduser(doctorModel), userController.AllPatientTestsbyDoctor);
+userRouter.patch("/doctor/updatePassword", userController.updatePassword(doctorModel));
+userRouter.post("/doctor/test", userController.addTest);
+userRouter.get("/doctor/test/:id", userController.getTest);
+userRouter.patch("/doctor/test/:id", userController.updateTest);
+userRouter.delete("/doctor/test/:id", userController.deleteTest);
+userRouter.get("/doctor/tests/:doctorid", userController.getTestsbyDoctor);
+userRouter.get("/doctor/Alltests/:patientid/:doctorid", userController.AllPatientTestsbyDoctor);
 
 
 
@@ -26,6 +28,7 @@ userRouter.get("/doctor/Alltests/:patientid/:doctorid", authUser, alloweduser(do
 
 // patient protected routes
 /////////////////////////////////
+application.use(authUser, alloweduser(patientModel));
 // patient gets certain test result
 userRouter.patch("/patient/updatePassword", authUser, alloweduser(patientModel), userController.updatePassword(patientModel));
 userRouter.get("/patient/test/:patientid/:testid", authUser, alloweduser(patientModel), userController.getPatientTest);
